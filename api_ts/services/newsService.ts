bb6788bdf552cd43efc6ff1b15d6422c222cf2a0
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-12 12:35:01 -0300

    NewsServwce
    create update

--- api_ts/services/newsService.ts
+++ api_ts/services/newsService.ts
@@ -8,6 +8,14 @@ class NewsService{
     getById(_id){
         return NewsRepository.findById(_id);
     }
+
+    create(news){
+        return NewsRepository.create(news);
+    }
+
+    update(_id,news){
+        return NewsRepository.findByIdAndUpdate(_id, news);
+    }
 }
 
 export default new NewsService();
