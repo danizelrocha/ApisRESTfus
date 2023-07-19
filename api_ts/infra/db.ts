d3c4f085f2e5bb4b8455ea17319f7eb9c9a89775
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-19 12:28:16 -0300

    db.ts

--- api_ts/infra/db.ts
+++ api_ts/infra/db.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 
 class Database {
-    private DB_URL = "mongodb://localhost:27017/bd_portal";
+    private DB_URL = "mongodb://link-db/bd_portal";
 
     createConnection() {
         mongoose.connect(this.DB_URL);
